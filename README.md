1421639ad965815eb8c662410e795328cb752317
andreasbehnke <ab@andreasbehnke.com>
2022-08-24 11:08:26 +0200

    added gas meter script

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# heating-system-monitoring
-Documentation and scripts of my home heating system monitoring. I use this tooling to identify potential savings of my gas heating system, which has never been fine tuned after installation.
+# Heating System Monitoring
+Documentation and scripts of my home heating system monitoring. I use this tooling to identify potential gas savings. The gas system has never been fine tuned after installation.
+
+# Heating Hardware
+
+* viessmann vitodens 222-F BS2B 2.4 - 26.0 kW
+* gas meter 
+
+# Monitoring Hardware
+
+* raspberry pi 3b
+* viessmann optolink USB cable
+* reed contact to count gas meter impulses
+
+# Monitoring Software
+
+## vcontrold
+
+The service vcontrold is used to collect system data via usb optolink cable.
+
+## gas meter counter
+
+* sudo apt install rpi.gpio-common 
+* sudo apt install python3-influxdb 
---
+++ src/gas-meter/gasmeter.py
@@ -0,0 +1,55 @@
+#!/usr/bin/python
+# read gas meter impulses from GPIO pin 23 and send time series data to influxdb.
+# Reed contact must switch pin 23 to GND, the internal pull up resistor will be used.
+#
+# Usage: gasmeter.py INFLUX_HOST INFLUX_USER_NAME INFLUX_USER_PASS INFLUX_DB_NAME
+
+import sys
+import time
+from influxdb import InfluxDBClient
+import RPi.GPIO as GPIO
+
+def readActual():
+	file = open('gasmeter.dat')
+	line = file.read()
+	file.close()
+	a = float(line)
+	return a
+def writeActual(a):
+	file = open('gasmeter.dat', 'w')
+	s = '{0:0.2f}'.format(a)
+	file.write(s)
+	file.close()
+def sendActual(client):
+	json_body = [{
+        "measurement": "heating_system_gas_meter_count",
+        "fields": {
+            "value": readActual()
+        }
+    }]
+	client.write_points(json_body)
+
+channel = 23 # connect GPIO 23 to reed contact, other end of reed contact to GND.
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+time.sleep(2)
+
+client = InfluxDBClient(sys.argv[1], 8086, sys.argv[2], sys.argv[3], sys.argv[4])
+
+seconds = 60
+impulse = not GPIO.input(channel) # retrieve the current pulse state
+
+while True:
+	if not GPIO.input(channel) and not impulse:
+		impulse = True # we are now within the pulse (duration around 4 seconds, or gas meter may also stop in this position)
+		gas_value = readActual()
+		gas_value = gas_value + 0.01
+		writeActual(gas_value)
+		seconds = 0
+	elif GPIO.input(channel) and impulse:
+		impulse = False
+	if seconds == 0:
+		sendActual()
+		seconds = 60
+	seconds = seconds - 1
+	time.sleep(1)
