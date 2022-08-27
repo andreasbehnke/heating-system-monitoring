eca3b4657c4eaeb0915786b957316f68601bab38
andreasbehnke <ab@andreasbehnke.com>
2022-08-27 18:01:07 +0200

    applied python formatting rules

--- src/gas-meter/gasmeter.py
+++ src/gas-meter/gasmeter.py
@@ -12,29 +12,35 @@ from systemd.journal import JournaldLogHandler
 from influxdb import InfluxDBClient
 import RPi.GPIO as GPIO
 
-def readActual():
-	file = open('gasmeter.dat')
-	line = file.read()
-	file.close()
-	a = float(line)
-	return a
-def writeActual(a):
-	file = open('gasmeter.dat', 'w')
-	s = '{0:0.2f}'.format(a)
-	file.write(s)
-	file.close()
-def sendActual(client):
-	json_body = [{
+
+def read_actual():
+    file = open('gasmeter.dat')
+    line = file.read()
+    file.close()
+    a = float(line)
+    return a
+
+
+def write_actual(a):
+    file = open('gasmeter.dat', 'w')
+    s = '{0:0.2f}'.format(a)
+    file.write(s)
+    file.close()
+
+
+def send_actual():
+    json_body = [{
         "measurement": "heating_system_gas_meter_count",
         "fields": {
-            "value": readActual()
+            "value": read_actual()
         }
     }]
-	client.write_points(json_body)
+    client.write_points(json_body)
 
-channel = 23 # connect GPIO 23 to reed contact, other end of reed contact to GND.
+
+channel = 23  # connect GPIO 23 to reed contact, other end of reed contact to GND.
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(channel, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+GPIO.setup(channel, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 time.sleep(2)
 
 log = logging.getLogger('gasmeter')
@@ -44,23 +50,23 @@ log.setLevel(logging.INFO)
 client = InfluxDBClient(sys.argv[1], 8086, sys.argv[2], sys.argv[3], sys.argv[4])
 
 seconds = 60
-impulse = not GPIO.input(channel) # retrieve the current pulse state
+impulse = not GPIO.input(channel)  # retrieve the current pulse state
 
 while True:
-	try:
-		if not GPIO.input(channel) and not impulse:
-			impulse = True # we are now within the pulse (duration around 4 seconds, or gas meter may also stop in this position)
-			gas_value = readActual()
-			gas_value = gas_value + 0.01
-			writeActual(gas_value)
-			seconds = 0 # trigger send if counter changes
-		elif GPIO.input(channel) and impulse:
-			impulse = False
-		if seconds == 0:
-			sendActual(client)
-			seconds = 60 # send once per minute
-		seconds = seconds - 1
-		time.sleep(1)
-	except BaseException as err:
-		log.exception("Exception in main thread, retry in 60 seconds")
-		seconds = 60
+    try:
+        if not GPIO.input(channel) and not impulse:
+            impulse = True  # we are now within the pulse (duration around 4 seconds, or gas meter may also stop in this position)
+            gas_value = read_actual()
+            gas_value = gas_value + 0.01
+            write_actual(gas_value)
+            seconds = 0  # trigger send if counter changes
+        elif GPIO.input(channel) and impulse:
+            impulse = False
+        if seconds == 0:
+            send_actual(client)
+            seconds = 60  # send once per minute
+        seconds = seconds - 1
+        time.sleep(1)
+    except BaseException as err:
+        log.exception("Exception in main thread, retry in 60 seconds")
+        seconds = 60
