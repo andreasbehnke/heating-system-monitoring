2d6ac0739afe95bd22a0882e894d353d9e41fbfd
andreasbehnke <ab@andreasbehnke.com>
2022-09-01 11:03:35 +0200

    create missing parent directories and ignore existing

--- src/gas-meter/install.sh
+++ src/gas-meter/install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 sudo apt install rpi.gpio-common python3-systemd python3-influxdb
-sudo mkdir /opt/gasmeter
+sudo mkdir -p /opt/gasmeter
 sudo cp gasmeter.py /opt/gasmeter
 sudo chmod 0700 /opt/gasmeter/gasmeter.py
 sudo cp gasmeter.service /etc/systemd/system
 sudo cp gasmeter_template.ini /etc/gasmeter.ini
 sudo chmod 0600 /etc/gasmeter.ini
-sudo mkdir /var/lib/gasmeter
+sudo mkdir -p /var/lib/gasmeter
 sudo touch /var/lib/gasmeter/gasmeter.dat
 sudo systemctl enable gasmeter
